64272655bd9c62742c52836927e901455288d84b
Anzil R <anzilraheem@gmail.com>
2024-09-10 20:04:14 +0300

    Changed player link

--- subedit/plugins/editor/adjust_time.py
+++ subedit/plugins/editor/adjust_time.py
@@ -149,7 +149,7 @@ async def inputNewTime(_, message):
                     InlineKeyboardButton(
                         text="📺 Player",
                         web_app=WebAppInfo(
-                            url=f"https://webplayer-aflw.onrender.com/player/{subtitle_id}/{index}/"
+                            url=f"https://eblayer-anzilr9398-jh1n7zan.leapcell.dev/player/{subtitle_id}/{index}/"
                         ),
                     )
                 ],
--- subedit/plugins/editor/line_pagination.py
+++ subedit/plugins/editor/line_pagination.py
@@ -86,7 +86,7 @@ async def paginateLine(query):
                 InlineKeyboardButton(
                     text="📺 Player",
                     web_app=WebAppInfo(
-                        url=f"https://webplayer-aflw.onrender.com/player/{sub_id}/{index}/"
+                        url=f"https://eblayer-anzilr9398-jh1n7zan.leapcell.dev/player/{sub_id}/{index}/"
                     ),
                 ),
             ],
