fde051d92fcd4071777fa38b10fffed3521a2fa8
Anzil R <anzilraheem@gmail.com>
2024-09-30 17:11:16 +0300

    Added collaborator feature

--- subedit/database/MongoDb.py
+++ subedit/database/MongoDb.py
@@ -1,4 +1,6 @@
 from pymongo.errors import ServerSelectionTimeoutError, DuplicateKeyError
+from pyrogram.filters import document
+
 from subedit.config import MONGO_URI, DB_NAME
 from subedit.logging import LOGGER
 import motor.motor_asyncio
@@ -90,6 +92,11 @@ class MongoDB:
         )
         return result.modified_count
 
+    async def add_collab_id(self, user_id, collab_data):
+        result = await self.users_collection.update_one(
+            {"_id": user_id}, {"$push": {"subtitles": collab_data}}
+        )
+
     async def get_sub_id(self, user_id):
         document = await self.users_collection.find_one(
             {"_id": user_id},
@@ -99,6 +106,13 @@ class MongoDB:
             },  # Projecting to only include the subtitles array
         )
         return document.get("subtitles") if document else None
+
+    async def get_sub_name(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"file_name": 1, "_id": 0}
+        )
+        return document.get("file_name") if document else None
 
     async def update_last_message_id(self, subtitle_id, message_id):
         result = await self.subtitles_collection.update_one(
@@ -112,6 +126,102 @@ class MongoDB:
             {"$set": {"last_message_id": message_id, "last_edited_line": index}},
         )
         return result.modified_count
+
+    async def update_collab_status(self, subtitle_id, status, user_id):
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id},
+            {"$set": {"collab_status": status, "collab_admin": user_id}},
+        )
+        return result.modified_count > 0
+
+    async def get_collab_status(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"collab_status": 1, "_id": 0}
+        )
+        return document.get("collab_status") if document else None
+
+    async def get_collab_admin(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"collab_admin": 1, "_id": 0}
+        )
+        return document.get("collab_admin") if document else None
+
+    async def update_collab_members(self, subtitle_id, user_data):
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id}, {"$push": {"collab_members": user_data}},
+        )
+        return result.modified_count > 0
+
+    async def get_collab_members(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"collab_members": 1, "_id": 0}
+        )
+        return document.get("collab_members") if document else None
+
+    async def get_collab_member(self, subtitle_id, user_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id, "collab_members.id": str(user_id)},
+            {"collab_members.$": 1, "_id": 0}
+        )
+        return document.get("collab_members", [{}])[0] if document else None
+
+    async def is_collab_member(self, subtitle_id, user_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id, "collab_members.id": str(user_id)},
+            {"_id": 1}  # We only need to know if the document exists
+        )
+        return document is not None
+
+    async def remove_collab_member(self, subtitle_id, user_id):
+        print(str(user_id))
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id}, {"$pull": {"collab_members": {"id": str(user_id)}}},
+        )
+        print(f"Removed member with id {user_id} from subtitles collection" if result.modified_count > 0 else "No member with id {user_id} found")
+        return result.modified_count > 0
+
+    async def remove_collab_data(self, subtitle_id):
+
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id}, {"$unset": {"collab_status": 1, "collab_members": 1}},
+        )
+        return result.modified_count > 0
+
+    async def update_collab_blacklist(self, subtitle_id, user_data):
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id}, {"$push": {"collab_blacklist": user_data}},
+        )
+        return result.modified_count > 0
+
+    async def get_collab_blacklist(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"collab_blacklist": 1, "_id": 0}
+        )
+        return document.get("collab_blacklist") if document else None
+
+    async def remove_collab_blacklist_user(self, subtitle_id, user_id):
+        result = await self.subtitles_collection.update_one(
+            {"_id": subtitle_id}, {"$pull": {"collab_blacklist": {"id": str(user_id)}}},
+        )
+        return result.modified_count > 0
+
+    async def get_collab_blacklist_user(self, subtitle_id, user_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id, "collab_blacklist.id": str(user_id)},
+            {"collab_blacklist.$": 1, "_id": 0}
+        )
+        return document.get("collab_blacklist", [{}])[0] if document else None
+
+    async def is_collab_member_blacklist(self, subtitle_id, user_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id, "collab_blacklist.id": str(user_id)},
+            {"_id": 1}  # We only need to know if the document exists
+        )
+        return document is not None
 
     async def update_last_edited_line(self, subtitle_id, index):
         result = await self.subtitles_collection.update_one(
@@ -133,6 +243,16 @@ class MongoDB:
         index = document.get("last_edited_line") if document else None
         message_id = document.get("last_message_id") if document else None
         return index, message_id
+
+    async def get_last_message_id_index_and_collab_status(self, subtitle_id):
+        document = await self.subtitles_collection.find_one(
+            {"_id": subtitle_id},
+            {"last_edited_line": 1, "last_message_id": 1, "collab_status": 1, "_id": 0},
+        )
+        index = document.get("last_edited_line") if document else None
+        message_id = document.get("last_message_id") if document else None
+        collab_status = document.get("collab_status") if document else None
+        return index, message_id, collab_status
 
     async def update_error_message_id(self, subtitle_id, message_id):
         result = await self.subtitles_collection.update_one(
