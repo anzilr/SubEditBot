032f0c5e743a47b9ae8d618ba7fe73db0f75ce4e
Anzil R <anzilraheem@gmail.com>
2024-09-30 17:10:43 +0300

    Added collaborator feature

--- subedit/plugins/editor/delete_subtitle.py
+++ subedit/plugins/editor/delete_subtitle.py
@@ -1,5 +1,5 @@
 from subedit import bot
-from subedit.database.database import deleteSubtitleDocument, getSubtitleID
+from subedit.database.database import deleteSubtitleDocument, getSubtitleID, getCollabAdmin
 from subedit.helpers.Filters.custom_filters import CallbackButtonDataFilter
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -8,27 +8,32 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def deleteSubtitle(_, query):
     sub_id = query.data.split("|")[1]
     user_id = query.from_user.id
-    await bot.edit_message_text(
-        chat_id=user_id,
-        message_id=query.message.id,
-        text="Deleting the subtitle will erase all the data from this session. Make sure you compiled the work before deleting.",
-        reply_markup=InlineKeyboardMarkup(
-            [
+    collab_admin = getCollabAdmin(sub_id)
+    if collab_admin != user_id:
+        await query.answer("⛔️ You are not authorized to delete this subtitle.", show_alert=True)
+        return
+    else:
+        await bot.edit_message_text(
+            chat_id=user_id,
+            message_id=query.message.id,
+            text="Deleting the subtitle will erase all the data from this session. Make sure you compiled the work before deleting.",
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        text="🚮 Delete",
-                        callback_data=f"DELETE_CURRENT_SUB|{sub_id}",
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="❎ Cancel",
-                        callback_data=f"CANCEL_DELETE_SUB|{sub_id}",
-                    )
-                ],
-            ]
-        ),
-    )
+                    [
+                        InlineKeyboardButton(
+                            text="🚮 Delete",
+                            callback_data=f"DELETE_CURRENT_SUB|{sub_id}",
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="❎ Cancel",
+                            callback_data=f"CANCEL_DELETE_SUB|{sub_id}",
+                        )
+                    ],
+                ]
+            ),
+        )
 
 
 @bot.on_callback_query(CallbackButtonDataFilter("DELETE_CURRENT_SUB"))
